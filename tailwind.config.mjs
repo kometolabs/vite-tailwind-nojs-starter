008d0b01cac5a559e145479bf3b9d62f6fff1f56
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-03-22 16:14:42 +0700

    Chaged config format to mjs. Enaled clearing the output build.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,4 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {
--- vite.config.mjs
+++ vite.config.mjs
@@ -6,6 +6,7 @@ export default defineConfig({
   },
   build: {
     outDir: "../dist",
+    emptyOutDir: true
   },
   root: "src",
 });
